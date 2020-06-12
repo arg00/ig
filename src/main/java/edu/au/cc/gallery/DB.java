1ba65998fdd972b176c29014d78c66bda5d434fa
arg <arg005@auburn.edu>
2020-06-12 15:33:51 +0000

    Initial commit.

---
+++ src/main/java/edu/au/cc/gallery/DB.java
@@ -0,0 +1,104 @@
+package edu.au.cc.gallery;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DB {
+
+    public DB() {
+    	//System.out.println("made new db");
+    }
+
+    private static final String dbUrl = "jdbc:postgresql://gallery-db.c5qjvlv2tflz.us-west-1.rds.amazonaws.com/image_gallery";
+
+    private Connection connection;
+    
+    public void connect() throws SQLException {
+	
+	try {
+	    //Class.forName("org.postgresql.Driver");
+	    //JSONObject secret = getSecret();
+	    connection = DriverManager.getConnection(dbUrl, "image_gallery", "Ziege666");
+	} catch (Exception ex) {
+	    ex.printStackTrace();
+	    System.exit(1);
+	}
+	
+    }
+
+    public ResultSet executeQuery(String query) throws SQLException {
+	PreparedStatement stmt = connection.prepareStatement(query);
+	ResultSet rs = stmt.executeQuery();
+	return rs;
+    }
+
+    public void execute(String query, String[] values) throws SQLException {
+	PreparedStatement stmt = connection.prepareStatement(query);
+	for(int i=0; i < values.length; i++)
+	    stmt.setString(i+1, values[i]);
+	stmt.execute();
+    }
+
+    public ResultSet executeQuery(String query, String[] values) throws SQLException {	
+        PreparedStatement stmt = connection.prepareStatement(query);
+
+	for(int i = 0; i < values.length; i++) 
+		stmt.setString(i+1, values[i]);
+	ResultSet rs = stmt.executeQuery();
+	return rs;
+    }
+
+    public void close() throws SQLException {
+	connection.close();
+    }
+
+    public void initDB() throws Exception {
+	this.connect();
+    }
+
+    public void listAll() throws Exception {
+	System.out.println("username\tpassword\tfull name");
+	System.out.println("----------------------------------------");
+	ResultSet rs = this.executeQuery("select username,password,full_name from users");
+	while(rs.next()) System.out.println(rs.getString(1)+"\t\t"+rs.getString(2)+"\t\t"+rs.getString(3));
+	rs.close();
+    }
+
+    public void addUser(String un, String pw, String fn) throws Exception {
+	String[] vals = {un, pw, fn};
+	String stmt = "insert into users\nvalues (?,?,?)";
+	this.execute(stmt, vals);
+    }
+
+    public ArrayList<String> getUsers() throws Exception {
+	ResultSet rs = this.executeQuery("select username from users");
+	ArrayList<String> userList = new ArrayList<String>();
+	while(rs.next()) userList.add(rs.getString(1));
+	return userList;
+    }
+
+    public void updateUser(String un, String pw, String fn) throws Exception {
+	String[] val = {un};
+	ResultSet rs = this.executeQuery("select * from users where username=?", val);
+	ArrayList<String> currentVals = new ArrayList<String>();
+	while(rs.next()) { currentVals.add(rs.getString(1)); currentVals.add(rs.getString(2)); currentVals.add(rs.getString(3));  }
+	if (pw.equals("")) pw = currentVals.get(1);
+	if (fn.equals("")) fn = currentVals.get(2);
+	String updtStmt = "update users\nset password = ?, full_name = ?\nwhere username = ?";
+	String[] stmtArgs = { pw, fn, un };
+	this.execute(updtStmt, stmtArgs);
+    }
+
+    public void deleteUser(String un) throws Exception {
+	String[] vals = { un };
+	this.execute("delete from users where username = ?", vals);
+    }
+}
---
+++ src/main/java/edu/au/cc/gallery/tools/UserAdmin.java
@@ -0,0 +1,94 @@
+package edu.au.cc.gallery.tools;
+
+import edu.au.cc.gallery.DB;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class UserAdmin {
+
+	public static void main(String[] args) throws Exception {
+	        DB db = new DB();
+		db.initDB();
+		
+		Scanner sc = new Scanner(System.in);
+                int input = -1;
+
+                do {
+			ArrayList<String> currUserList = db.getUsers();
+			printOptions();
+			input = Integer.parseInt(sc.nextLine());
+			switch (input) {
+				case 1:
+					db.listAll();
+					break;
+				case 2: 
+					System.out.println("Username> ");
+					String userAddName = sc.nextLine();
+					if (currUserList.contains(userAddName)) {
+						System.out.println("Error: user with username "+userAddName+" already exists.");
+						break;
+					}
+					String[] addVals = addOptions();
+					db.addUser(userAddName, addVals[0], addVals[1]);
+					break;
+				case 3:
+					System.out.print("Username to edit> ");
+					String toEdit = sc.nextLine();
+					if (!currUserList.contains(toEdit)) {
+						System.out.println("No such user.");
+						break;
+					}
+					String[] updateVals = updateOptions();
+					db.updateUser(toEdit, updateVals[0], updateVals[1]);
+					break;
+				case 4:
+					System.out.println("Enter username to delete> ");
+					String toDelete = sc.nextLine();
+					if (!currUserList.contains(toDelete)) {			
+						System.out.println("No such user.");
+						break;
+					}
+					db.deleteUser(toDelete);
+					break;
+			}
+		
+		} while (input != 5);
+		
+		System.out.println("Bye.");
+
+		db.close();
+	}	
+
+	private static void printOptions() {
+		String output = "1) List Users\n";
+		output += "2) Add user\n";
+		output += "3) Edit user\n";
+		output += "4) Delete user\n";
+		output += "5) Quit\n";
+		output += "Enter command> ";
+
+		System.out.println(output); 
+	}
+
+	public static String[] addOptions() {
+		Scanner sc2 = new Scanner(System.in);
+
+		String[] vals = new String[2];
+		System.out.println("Password> ");
+		vals[0] = sc2.nextLine();
+		System.out.println("Full name> ");
+		vals[1] = sc2.nextLine();
+		return vals;		
+	}
+
+	public static String[] updateOptions() {
+		Scanner sc3 = new Scanner(System.in);
+
+		String[] vals = new String[2];
+		System.out.println("New password (press enter to keep current)>");
+		vals[0] = sc3.nextLine();
+		System.out.println("New full name (press enter to keep current)>");
+		vals[1] = sc3.nextLine();
+		return vals;
+	}
+}
